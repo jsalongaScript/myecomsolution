fdd1f282b856a7da72ee28429c632558661ae546
jsalongaScript <joanasalonga87@gmail.com>
2024-09-17 15:14:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const navsearch = document.getElementById("nav-search");
+
+navsearch.addEventListener("click" , (e) => {
+    navsearch.classList.toggle("open");
+})
+
+ScrollReveal().reveal('.hero-content', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    reset: true,
+});
